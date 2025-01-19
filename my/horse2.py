07d5b8fa855f93d70105e34509478db1c6a72dfa
nektol4 <nekit300707@gmail.com>
2025-01-19 23:27:56 +0900

    Add files via upload

--- my/horse2.py
+++ my/horse2.py
@@ -1,10 +1,10 @@
 
-def horse2(pos):
-    x, y = pos
-
-    moves = [
-        (2, 1), (2, -1), (-2, 1), (-2, -1),
-        (1, 2), (1, -2), (-1, 2), (-1, -2)
-    ]
-    for move in moves:
-        print(move)
+def horse2(cell):
+    alpha = 'abcdefgh'
+    moves = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
+    x = alpha.find(cell[0]) + 1
+    y = int(cell[1])
+    for xy in moves:
+        x1, y1 = x + xy[0], y + xy[1]
+        if 0 < x1 < 9 and 0 < y1 < 9:
+            print(f'{alpha[x1-1]}{y1}')
