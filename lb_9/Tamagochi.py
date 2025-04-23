1f5c904e16aba567d46e2180531e6ec492a10574
nektol4 <nekit300707@gmail.com>
2025-04-23 09:32:10 +0900

    Update Tamagochi.py

--- lb_9/Tamagochi.py
+++ lb_9/Tamagochi.py
@@ -2,40 +2,62 @@ class tamagochi:
     def __init__(self, name):
         self.name = name
         self.hunger = 30
-        self.energy = 100
+        self.energy = 70
         self.boring = 30
+        self.life = True
 
     def feed(self):
+        if not self.life:
+            print(f'{self.name} мертв')
+            return
+
         if self.hunger > 0:
             self.hunger -= 10
-            return f'вы покормили {self.name}. уровень голода: {self.hunger}'
-        if self.hunger <= 90:
-            return f'срочно покормите {self.name}'
         else:
-            return f'{self.name} не может переесть'
+            print(f'{self.name} не хочет есть')
+            self.status()
 
     def sleep(self):
-        if self.enegy < 100:
-            self.energy += 40
-            return f'{self.name} поспал. уровень энергии: {self.energy}'
+        if not self.life:
+            print(f'{self.name} мертв.')
+            return
+
+        if self.energy < 100:
+            self.energy += 20
         else:
-            return f'{self.name} не хочет спать'
+            print(f'{self.name} не хочет спать')
+        self.status()
 
     def play(self):
-        if self.energy < 100:
+        if not self.life:
+            print(f'{self.name} мертв')
+            return
+
+        if self.energy > 20 and self.boring > 0:
             self.energy -= 20
             self.hunger += 20
-            self.boring -= 30
-            return f'вы поиграли с {self.name}. уровень энерегии {self.energy}. уровень голода {self.hunger}. уровень унылости {self.boring}'
+            self.boring -= 10
         else:
-            return f'{self.name} не хочет играть'
+            print(f'{self.name} не хочет играть')
+        self.status()
 
     def status(self):
-        if self.hunger and self.boring == 100:
-            return f'{self.name} умер.'
-        else:
-            return f'{self.name} чувствует себя нормально'
+        if not self.life:
+            print(f'{self.name} мертв.')
+            return
 
-a = tamagochi('дфкн')
-a.hunger
-a.status()
+        if self.hunger >= 100 or self.boring <= 0 or self.energy <= 0:
+            print(f'{self.name} в плохом состоянии.')
+            if self.hunger >= 100:
+                print(f'{self.name} голоден')
+                self.life = False
+            if self.boring <= 0:
+                print(f'{self.name} скучает')
+                self.life = False
+            if self.energy <= 0:
+                print(f'{self.name} устал')
+                self.life = False
+        else:
+            print(f'Энергия: {self.energy}, Голод: {self.hunger}, Скука: {self.boring}')
+
+
